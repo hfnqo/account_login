30a483324c191be37141694f625d18307be64b04
hfnqo <joushitsu22@gmail.com>
2023-03-23 17:27:32 +0800

    feat: Add welcome page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Account = require('../../models/account')
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  Account.findOne({ email: email, password: password })
+    .lean()
+    .then(account => res.render('welcome', { account }))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
