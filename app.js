7050a06e001f57f4097cc80b401b26ec48b9d942
hfnqo <joushitsu22@gmail.com>
2023-03-29 17:56:05 +0800

    feat: Add login with cookies function

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 
-const routes = require('./routes')
 require('./config/mongoose')
+
+const routes = require('./routes')
 const app = express()
+const port = 3000
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`)
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,20 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const cookieParser = require('cookie-parser')
+
 const Account = require('../../models/account')
 
+router.use(cookieParser())
+
 router.get('/', (req, res) => {
-  res.render('index')
+  const { userId } = req.cookies
+
+  if (!userId) res.render('index')
+
+  Account.findOne({_id: userId})
+    .lean()
+    .then(account => res.render('welcome', { account }))
+    .catch(error => console.log(error))
 })
 
 router.post('/', (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
+  const { email, password } = req.body
+  
+  if (!{ email, password }) return res.redirect('/')
 
-  Account.findOne({ email: email, password: password })
+  Account.findOne({ email, password })
     .lean()
     .then(account => {
-      if (!account) res.render('error')
-      
+      res.cookie('userId', account._id)
       res.render('welcome', { account })
     })
     .catch(error => console.log(error))
